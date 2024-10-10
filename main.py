d2d02d6b7df24d7c38aebe1c97000de8c94d270b
edkrueger <edkrueger@gmail.com>
2024-10-10 16:02:05 -0500

    finish prompt choice

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 def is_valid(choice):
+
+    if choice == None:
+        return False
     if not choice.lower() in {"r", "p", "s"}:
         return False
     return True
 
+def prompt_choice():
+
+    choice = None
+
+    while not is_valid(choice):
+        choice = input("Choose r/p/s! ")
+
+    return choice
+
 if __name__ == "__main__":
     assert is_valid("r")
     assert not is_valid("asdf")
+
+    prompt_choice()
